91cbf55b7cabcee77dc6f007f6269a7ecd63762f
Zixun Hao <zh1061@nyu.edu>
2024-06-15 20:51:06 -0400

    try to understand fold in python

--- explore_fold.py
+++ explore_fold.py
@@ -1,5 +1,13 @@
 import functools 
 
+def dictify_(dict):
+    result = {}
+    for k, v in dict:
+        result.update({k: v})
+    return result
+dict_a = dictify_([("A", 1), ("B", 2), ("C", 3)])
+print(dict_a)
+
 result = functools.reduce(lambda acc, item: acc + item, [1, 2, 3, 4, 5])
 result1 = functools.reduce(lambda acc, item: acc * item, [1, 2, 3, 4, 5])
 
