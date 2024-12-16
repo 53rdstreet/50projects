ac4d20823bb51539a35e36343c12c4ce0c41318a
Zixun Hao <zh1061@nyu.edu>
2024-12-16 15:10:20 -0500

    for python_code in private git

--- queries_migration.sql
+++ queries_migration.sql
@@ -1,2 +1,8 @@
 update public.tbl_race_analysis set "RaceAnalysis" = 'Black'
-where "Username" = 'aua2104'
+where "Username" = 'aua2104';
+
+update public.tbl_race_analysis set "RaceAnalysis" = 'White'
+where "Username" = 'sa4185';
+
+update public.tbl_race_analysis set "RaceAnalysis" = 'Asian'
+where "Username" = 'ag4664';
